cc447a7bc55a148345e8f2f2683160960be5e020
Marc Balaban <marc@marcbalaban.com>
2017-05-11 16:30:57 -0700

    [RASTER] enabling density accumulation on layers

--- src/utils/utils-vega.js
+++ src/utils/utils-vega.js
@@ -1,5 +1,14 @@
 export function notNull (value) { return value != null /* double-equals also catches undefined */ }
 
+function convertHexToRGBA (hex, opacity) {
+  hex = hex.replace("#", "")
+  const r = parseInt(hex.substring(0, 2), 16)
+  const g = parseInt(hex.substring(2, 4), 16)
+  const b = parseInt(hex.substring(4, 6), 16)
+
+  return `rgba(${r},${g},${b},${opacity / 100})`
+}
+
 export function createVegaAttrMixin (layerObj, attrName, defaultVal, nullVal, useScale, prePostFuncs) {
   let scaleFunc = "", fieldAttrFunc = ""
   const capAttrName = attrName.charAt(0).toUpperCase() + attrName.slice(1)
@@ -16,7 +25,7 @@ export function createVegaAttrMixin (layerObj, attrName, defaultVal, nullVal, us
 
     layerObj["_build" + capAttrName + "Scale"] = function (chart, layerName) {
       const scale = layerObj[scaleFunc]()
-      if (scale && scale.domain && scale.domain().length && scale.range().length) {
+      if (scale && scale.domain && scale.domain().length && scale.range().length && scaleFunc === "fillColorScale") {
         const colorScaleName = layerName + "_" + attrName
         const rtnObj = {
           name: colorScaleName,
@@ -29,6 +38,26 @@ export function createVegaAttrMixin (layerObj, attrName, defaultVal, nullVal, us
 
         if (scale.clamp) {
           rtnObj.clamp = scale.clamp()
+        }
+
+        return rtnObj
+      } else if (layerObj.densityAccumulatorEnabled()) {
+        const
+          OPACITY_LEVEL = 90,
+          ONE_PERCENT = 0.01,
+          colorScaleName = layerName + "_" + attrName,
+          selectedColors = layerObj.defaultFillColor(),
+          domainInterval = 100 / (selectedColors.length - 1),
+          logScale = selectedColors.map((color, i) => i ? i * domainInterval / 100 : ONE_PERCENT)
+
+        const rtnObj = {
+          name: colorScaleName,
+          type: "log",
+          domain: logScale,
+          range: selectedColors.map((color, i) => convertHexToRGBA(color, i + OPACITY_LEVEL)),
+          accumulator: "density",
+          minDensityCnt: "-2ndStdDev",
+          maxDensityCnt: "2ndStdDev"
         }
 
         return rtnObj
